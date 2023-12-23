f11c2a916b420c672a0b6dba8830d76bbe549c41
Sergey <>
2023-12-23 10:01:09 +0400

    add selectCityTextField

---
+++ SimpleWeatherApp/SimpleWeatherApp/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  SimpleWeatherApp
+//
+//  Created by Sergey on 23.12.2023.
+//
+
+import Foundation
+
+enum Constants {
+    static let fontSize_20: CGFloat = 20.0
+    static let borderWidth_2: CGFloat = 2.0
+    static let cornerRadius_5: CGFloat = 5.0
+    
+    static let constant_50: CGFloat = 50.0
+    static let constant_100: CGFloat = 100.0
+    static let constant_200: CGFloat = 200.0
+    
+    static let cityNamePlaceholder = "Enter city name"
+}
--- SimpleWeatherApp/SimpleWeatherApp/View/ViewController.swift
+++ SimpleWeatherApp/SimpleWeatherApp/View/ViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let selectCityTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = .clear
+        textField.textColor = .white
+        textField.textAlignment = .center
+        textField.font = UIFont.systemFont(ofSize: Constants.fontSize_20)
+        textField.placeholder = Constants.cityNamePlaceholder
+        textField.layer.borderWidth = Constants.borderWidth_2
+        textField.layer.borderColor = UIColor.white.cgColor
+        textField.layer.cornerRadius = Constants.cornerRadius_5
+        return textField
+    }()
+    
+    let cityNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .clear
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: Constants.fontSize_20)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemCyan
+        
+        setupScreen()
     }
-
-
 }
 
+private extension ViewController {
+    func setupScreen() {
+        setupSelectCityTextField()
+    }
+    
+    func setupSelectCityTextField() {
+        view.addSubview(selectCityTextField)
+        selectCityTextField.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(Constants.constant_100)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(Constants.constant_200)
+            make.height.equalTo(Constants.constant_50)
+        }
+    }
+    
+}
